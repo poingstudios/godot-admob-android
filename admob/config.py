df36c418ac060fc4617eb01d1f9682622fea37c9
Gustavo Maciel <gustamaciel2012@hotmail.com>
2020-05-09 22:52:33 -0300

    Banner fully working!

--- admob/config.py
+++ admob/config.py
@@ -4,3 +4,7 @@ def can_build(env, plat):
 def configure(env):
 	if env['platform'] == 'android':
 		env.android_add_java_dir("src")
+		#3.1.2 will only use 17.2.1, because on > 18.0.0, play-services-ads uses androidX, and 3.1.2 wasnt build for it
+		env.android_add_dependency("compile ('com.google.android.gms:play-services-ads:17.2.1') { exclude group: 'com.android.support' }") #exclude group due: Android导入多个第三方库导致 Duplicate class的问题排查思路: https://muka.app/?p=270
+		env.android_add_to_manifest("src/AndroidManifest.xml")
+		#initial config (https://developers.google.com/admob/android/quick-start)
