240a1d8e59a783c37ae08d2c0536ec6354155c20
Gustavo Maciel <gustamaciel2012@hotmail.com>
2020-05-13 18:45:08 -0300

    Prepare for iOS module

--- admob/config.py
+++ admob/config.py
@@ -1,5 +1,5 @@
 def can_build(env, plat):
-	return plat=="android" 
+	return plat=="android" or platform=="iphone"
 
 def configure(env):
 	if env['platform'] == 'android':
@@ -9,3 +9,5 @@ def configure(env):
 		env.android_add_dependency("compile ('com.google.android.gms:play-services-ads:17.2.1') { exclude group: 'com.android.support' }") #exclude group due: Android导入多个第三方库导致 Duplicate class的问题排查思路: https://muka.app/?p=270
 		env.android_add_to_manifest("android/manifests/AndroidManifest.xml")
 		#initial config (https://developers.google.com/admob/android/quick-start)
+	elif env['platform'] == 'iphone':
+		pass
