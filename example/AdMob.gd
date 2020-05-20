18f374cb2c5f83963dfb7f6bf9a597bbead3716a
Gustavo Maciel <gustamaciel2012@hotmail.com>
2020-05-19 22:43:06 -0300

    Update example
    Added unified native

--- example/AdMob.gd
+++ example/AdMob.gd
@@ -26,32 +26,43 @@ signal unified_native_ad_loaded
 signal unified_native_destroyed
 signal unified_native_ad_failed_to_load(error_code)
 signal unified_native_opened
-signal unified_native_clicked
-signal unified_native_left_application
 signal unified_native_closed
 
 
 var initialized := false
 
-var ad_unit_id = {
+onready var ad := {
 	"banner" : {
-		"iOS" : "ca-app-pub-3940256099942544/2934735716",
-		"Android" : "ca-app-pub-3940256099942544/6300978111"
+		"unit_id": {
+			"iOS" : "ca-app-pub-3940256099942544/2934735716",
+			"Android" : "ca-app-pub-3940256099942544/6300978111",
+		},
+		"loaded" : false,
 	},
 	"interstitial" : {
-		"iOS" : "",
-		"Android" : "ca-app-pub-3940256099942544/1033173712"
+		"unit_id": {
+			"iOS" : "",
+			"Android" : "ca-app-pub-3940256099942544/1033173712",
+		},
 	},
 	"rewarded" : {
-		"iOS" : "",
-		"Android" : "ca-app-pub-3940256099942544/5224354917"
+		"unit_id": {
+			"iOS" : "",
+			"Android" : "ca-app-pub-3940256099942544/5224354917",
+		},
 	},
 	"unified_native" : {
-		"iOS" : "",
-		"Android" : "ca-app-pub-3940256099942544/2247696110"
+		"unit_id": {
+			"iOS" : "",
+			"Android" : "ca-app-pub-3940256099942544/2247696110",
+		},
+		"loaded" : false,
+		"scale" : {
+			"x" : OS.get_screen_size().x / get_viewport_rect().size.x,
+			"y" : OS.get_screen_size().y / get_viewport_rect().size.y,
+		}
 	}
 }
-
 
 const GRAVITY = {
 	"TOP" : 48,
@@ -59,20 +70,12 @@ const GRAVITY = {
 	"CENTER" : 17,
 	"NO_GRAVITY" : 0
 }
-var test_device_id := {
-	"REAL_DEVICE" : OS.get_unique_id().md5_text().to_upper(),
-	"EMULATOR_DEVICE" : "B3EEABB8EE11C2BE770B684D95219ECB"
-}
 
-onready var SCALE = {
-	"x" : OS.get_screen_size().x / get_viewport_rect().size.x,
-	"y" : OS.get_screen_size().y / get_viewport_rect().size.y
+var test_device_id := {
+	"REAL_DEVICE" : OS.get_unique_id().md5_text().to_upper()
 }
 
 var AdMob
-
-var banner_loaded
-var banner_unit_id
 
 func _ready():
 	if (Engine.has_singleton("AdMob")):
@@ -86,51 +89,56 @@ func init(is_for_child_directed_treatment := true, is_personalized := false, max
 		print("init on the gdscript code!")
 		initialized = !initialized
 
-func load_banner(unit_id : String, gravity : int = GRAVITY.BOTTOM, size : String = "SMART_BANNER"):
+func load_banner(gravity : int = GRAVITY.BOTTOM, size : String = "SMART_BANNER", unit_id : String = ad.banner.unit_id[OS.get_name()]):
 	if AdMob:
 		AdMob.load_banner(unit_id, gravity, size)
-		banner_loaded = true
-		banner_unit_id = unit_id
+		ad.banner.loaded = true
+		ad.unified_native.loaded = false
 
+func load_interstitial(unit_id: String = ad.interstitial.unit_id[OS.get_name()]):
+	if AdMob:
+		AdMob.load_interstitial(unit_id)
+
+func load_rewarded(unit_id : String = ad.rewarded.unit_id[OS.get_name()]):
+	if AdMob:
+		AdMob.load_rewarded(unit_id)
+
+func load_unified_native(control_node_to_be_replaced : Control = Control.new(), unit_id : String = ad.unified_native.unit_id[OS.get_name()]):
+	if AdMob:
+		var params := {
+			"size" : {
+				"w" : control_node_to_be_replaced.rect_size.x * ad.unified_native.scale.x,
+				"h" : control_node_to_be_replaced.rect_size.y * ad.unified_native.scale.y
+			},
+			"position" : {
+				"x" : control_node_to_be_replaced.rect_position.x * ad.unified_native.scale.x,
+				"y" : control_node_to_be_replaced.rect_position.y * ad.unified_native.scale.y
+			}
+		}
+
+		ad.banner.loaded = false
+		ad.unified_native.loaded = true
+		AdMob.load_unified_native(unit_id, [params.size.w, params.size.h], [params.position.x, params.position.y])
+		
 func destroy_banner():
 	if AdMob:
 		AdMob.destroy_banner()
 
-func load_interstitial(unit_id: String):
+func destroy_unified_native():
 	if AdMob:
-		AdMob.load_interstitial(unit_id)
+		AdMob.destroy_unified_native()
 
-func load_rewarded(unit_id : String):
-	if AdMob:
-		AdMob.load_rewarded(unit_id)
-
-func load_unified_native(unit_id : String, control_node_to_be_replaced : Control):
-	if AdMob:
-		var ad := {
-			"size" : {
-				"w" : control_node_to_be_replaced.rect_size.x * SCALE.x,
-				"h" : control_node_to_be_replaced.rect_size.y * SCALE.y
-			},
-			"position" : {
-				"x" : control_node_to_be_replaced.rect_position.x * SCALE.x,
-				"y" : control_node_to_be_replaced.rect_position.y * SCALE.y
-			}
-		}
-		AdMob.load_unified_native(unit_id, [ad.size.w, ad.size.h], [ad.position.x, ad.position.y])
-		
 func show_interstitial():
 	if AdMob:
 		AdMob.show_interstitial()
-		if banner_loaded: destroy_banner()
 
 func show_rewarded():
 	if AdMob:
 		AdMob.show_rewarded()
-		if banner_loaded: destroy_banner()
 
 func _on_get_tree_resized():
 	if AdMob:
-		SCALE = {
+		ad.unified_native.scale = {
 			"x" : OS.get_screen_size().x / get_viewport_rect().size.x,
 			"y" : OS.get_screen_size().y / get_viewport_rect().size.y
 		}
@@ -173,7 +181,8 @@ func _on_AdMob_interstitial_left_application():
 
 func _on_AdMob_interstitial_closed():
 	emit_signal("interstitial_closed")
-	if banner_loaded: load_banner(banner_unit_id)
+	if ad.banner.loaded: load_banner()
+	if ad.unified_native.loaded: load_banner()
 
 func _on_AdMob_rewarded_ad_loaded():
 	emit_signal("rewarded_ad_loaded")
@@ -186,6 +195,8 @@ func _on_AdMob_rewarded_ad_opened():
 	
 func _on_AdMob_rewarded_ad_closed():
 	emit_signal("rewarded_ad_closed")
+	if ad.banner.loaded: load_banner()
+	if ad.unified_native.loaded: load_banner()
 
 func _on_AdMob_user_earned_rewarded(currency : String, amount : int):
 	emit_signal("rewarded_user_earned_rewarded", currency, amount)
@@ -193,24 +204,17 @@ func _on_AdMob_user_earned_rewarded(currency : String, amount : int):
 func _on_AdMob_rewarded_ad_failed_to_show(error_code : int):
 	emit_signal("rewarded_ad_failed_to_show", error_code)
 
-
-func _on_AdMob_unified_native_ad_loaded():
-	emit_signal("unified_native_ad_loaded")
+func _on_AdMob_unified_native_loaded():
+	emit_signal("unified_native_loaded")
 
 func _on_AdMob_unified_native_destroyed():
 	emit_signal("unified_native_destroyed")
 
-func _on_AdMob_unified_native_ad_failed_to_load(error_code : int):
-	emit_signal("unified_native_ad_failed_to_load", error_code)
+func _on_AdMob_unified_native_failed_to_load(error_code : int):
+	emit_signal("unified_native_failed_to_load", error_code)
 
 func _on_AdMob_unified_native_opened():
 	emit_signal("unified_native_opened")
-
-func _on_AdMob_unified_native_clicked():
-	emit_signal("unified_native_clicked")
-
-func _on_AdMob_unified_native_left_application():
-	emit_signal("unified_native_left_application")
 	
 func _on_AdMob_unified_native_closed():
 	emit_signal("unified_native_closed")
