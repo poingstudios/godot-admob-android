0874ce18bf0365b7dc3a4adb60846a0f37863b14
Gustavo Maciel <gustamaciel2012@hotmail.com>
2020-05-22 11:47:32 -0300

    Update iOS version Banner

--- admob/ios/src/AdMobBanner.h
+++ admob/ios/src/AdMobBanner.h
@@ -1,23 +1,17 @@
-#import <GoogleMobileAds/GoogleMobileAds.h>
 #import <GoogleMobileAds/GADBannerView.h>
-
 #import "app_delegate.h"
-#import "object.h"
-
+ 
 @interface AdMobBanner: NSObject <GADBannerViewDelegate> {
-    GADBannerView *_banner_view;
-    NSString *_test_device_id;
-    int _instance_id;
-    bool _initialized;
-
-    ViewController *_root_controller;
-
-    int _gravity;
-    NSString *_size;
+    GADBannerView *bannerView;
+    bool initialized;
+    bool isReal;
+    bool isOnTop;
+    int instanceId;
+    NSString *adUnitId;
+    ViewController *rootController;
 }
 
-- (void)initialize :(int)instance_id :(NSString*)test_device_id;
-- (void)load_banner :(NSString*)ad_unit_id :(int)gravity :(NSString*)size;
-- (void)destroy_banner;
+- (void)initialize:(BOOL)is_real: (int)instance_id;
+- (void)load_banner:(NSString*)ad_unit_id :(int)gravity :(NSString*)size;
 
 @end
