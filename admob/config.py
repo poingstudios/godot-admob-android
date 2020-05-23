b19d48d32a3ea9736084a8c09b643494f2692f8c
Gustavo Maciel <gustamaciel2012@hotmail.com>
2020-05-23 20:18:51 -0300

    #10 Fixed: Android version now working for 3.2.1

--- admob/config.py
+++ admob/config.py
@@ -1,17 +1,8 @@
 def can_build(env, plat):
-	return plat=="android" or plat=="iphone"
+	return plat=="iphone"
 
 def configure(env):
-	if env['platform'] == 'android':
-		env.android_add_java_dir("android/java")
-		env.android_add_res_dir("android/res")
-		#3.1.2 will only use 17.2.1, because on > 18.0.0, play-services-ads uses androidX, and 3.1.2 wasnt build for it
-		env.android_add_dependency("compile ('com.google.android.gms:play-services-ads:17.2.1') { exclude group: 'com.android.support' }") #exclude group due: https://muka.app/?p=270
-		env.android_add_to_manifest("android/manifests/AndroidManifest.xml")
-		#initial config (https://developers.google.com/admob/android/quick-start)
-		env.disable_module()
-		#disable module due iphone register_types
-	elif env['platform'] == 'iphone':
+	if env['platform'] == 'iphone':
 		env.Append(FRAMEWORKPATH=['#modules/admob/ios/lib'])
 		env.Append(CPPPATH=['#core'])
 		env.Append(LINKFLAGS=['-ObjC', '-framework','AdSupport', '-framework','CoreTelephony', '-framework','EventKit', '-framework','EventKitUI', '-framework','MessageUI', '-framework','StoreKit', '-framework','SafariServices', '-framework','CoreBluetooth', '-framework','AssetsLibrary', '-framework','CoreData', '-framework','CoreLocation', '-framework','CoreText', '-framework','ImageIO', '-framework', 'GLKit', '-framework','CoreVideo', '-framework', 'CFNetwork', '-framework', 'MobileCoreServices', '-framework', 'GoogleMobileAds'])
