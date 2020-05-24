2834abd916f8a3c1f4e09b3f860033afbac7623e
Gustavo Maciel <gustamaciel2012@hotmail.com>
2020-05-24 13:24:39 -0300

    Back to the old version (82e2d8ab5847140f91e0c1b1ae166a5070f03e52)

--- admob/ios/src/AdMobInterstitial.h
+++ admob/ios/src/AdMobInterstitial.h
@@ -1,17 +1,17 @@
 #import <GoogleMobileAds/GADInterstitial.h>
 #import "app_delegate.h"
-
-@interface AdMobInterstitial :NSObject <GADInterstitialDelegate> 
-{
-	GADInterstitial *interstitial;
-	bool initialized;
-	int instanceId;
-	NSString *adUnitId;
-	ViewController *rootController;
+ 
+@interface AdMobInterstitial: NSObject <GADInterstitialDelegate> {
+    GADInterstitial *interstitial;
+    bool initialized;
+    bool isReal;
+    int instanceId;
+    NSString *adUnitId;
+    ViewController *rootController;
 }
 
-- (void)initialize :(int)instance_id;
-- (void)load_interstitial :(NSString*)ad_unit_id;
+- (void)initialize:(BOOL)is_real: (int)instance_id;
+- (void)load_interstitial: (NSString*)ad_unit_id;
 - (void)show_interstitial;
 
 @end
--- admob/ios/src/AdMobRewarded.h
+++ admob/ios/src/AdMobRewarded.h
@@ -1,17 +1,17 @@
 #import <GoogleMobileAds/GADRewardedAd.h>
 #import "app_delegate.h"
-
-@interface AdMobRewarded: NSObject <GADRewardedAdDelegate> 
-{
-	GADRewardedAd *rewarded;
-	bool initialized;
-	int instanceId;
-	NSString *adUnitId;
-	ViewController *rootController;
+ 
+@interface AdMobRewarded: NSObject <GADRewardedAdDelegate> {
+    GADRewardedAd *rewarded;
+    bool initialized;
+    bool isReal;
+    int instanceId;
+    NSString *adUnitId;
+    ViewController *rootController;
 }
 
-- (void)initialize :(int)instance_id;
-- (void)load_rewarded :(NSString*)ad_unit_id;
+- (void)initialize:(BOOL)is_real: (int)instance_id;
+- (void)load_rewarded: (NSString*)ad_unit_id;
 - (void)show_rewarded;
 
 @end
