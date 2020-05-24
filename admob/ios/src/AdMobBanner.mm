29f4d2ad2befbfc68b99bad41bcab98ca4808918
Gustavo Maciel <gustamaciel2012@hotmail.com>
2020-05-23 22:35:48 -0300

    Reindent the code

--- admob/ios/src/AdMobBanner.mm
+++ admob/ios/src/AdMobBanner.mm
@@ -5,77 +5,77 @@
 
 - (void)dealloc 
 {
-    bannerView.delegate = nil;
-    [bannerView release];
-    [super dealloc];
+	bannerView.delegate = nil;
+	[bannerView release];
+	[super dealloc];
 }
 
 - (void)initialize :(int)instance_id :(NSString*)test_device_id 
 {
-    initialized = true;
-    instanceId = instance_id;
-    testDeviceId = test_device_id;
-    rootController = [AppDelegate getViewController];
+	initialized = true;
+	instanceId = instance_id;
+	testDeviceId = test_device_id;
+	rootController = [AppDelegate getViewController];
 }
 
 
 - (void) load_banner :(NSString*)ad_unit_id :(int)gravity :(NSString*)size 
 {
-    NSLog(@"Calling load_banner");
-    
-    isOnTop = false;
-    
-    if (!initialized || (!ad_unit_id.length)) 
-    {
-        return;
-    }
-    else
-    {
-        NSLog(@"banner will load with the banner id");
-        NSLog(ad_unit_id);
-    }
-    
+	NSLog(@"Calling load_banner");
+	
+	isOnTop = false;
+	
+	if (!initialized || (!ad_unit_id.length)) 
+	{
+		return;
+	}
+	else
+	{
+		NSLog(@"banner will load with the banner id");
+		NSLog(ad_unit_id);
+	}
+	
 
-    UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
-    
-    if (bannerView == nil) 
-    {
-        if ([size isEqualToString:@"BANNER"]) 
-        {
-            bannerView = [[GADBannerView alloc] initWithAdSize:kGADAdSizeBanner];
-            NSLog(@"Banner created");
-        } 
-        else if ([size isEqualToString:@"LARGE_BANNER"]) 
-        {
-            bannerView = [[GADBannerView alloc] initWithAdSize:kGADAdSizeLargeBanner];
-            NSLog(@"Large banner created");
-        }
-        else if ([size isEqualToString:@"MEDIUM_RECTANGLE"]) 
-        {
-            bannerView = [[GADBannerView alloc] initWithAdSize:kGADAdSizeMediumRectangle];
-            NSLog(@"Medium banner created");
-        } 
-        else if ([size isEqualToString:@"FULL_BANNER"]) 
-        {
-            bannerView = [[GADBannerView alloc] initWithAdSize:kGADAdSizeFullBanner];
-            NSLog(@"Full banner created");
-        } 
-        else if ([size isEqualToString:@"LEADERBOARD"]) 
-        {
-            bannerView = [[GADBannerView alloc] initWithAdSize:kGADAdSizeLeaderboard];
-            NSLog(@"Leaderboard banner created");
-        } 
-        else 
+	UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+	
+	if (bannerView == nil) 
+	{
+		if ([size isEqualToString:@"BANNER"]) 
+		{
+			bannerView = [[GADBannerView alloc] initWithAdSize:kGADAdSizeBanner];
+			NSLog(@"Banner created");
+		} 
+		else if ([size isEqualToString:@"LARGE_BANNER"]) 
+		{
+			bannerView = [[GADBannerView alloc] initWithAdSize:kGADAdSizeLargeBanner];
+			NSLog(@"Large banner created");
+		}
+		else if ([size isEqualToString:@"MEDIUM_RECTANGLE"]) 
+		{
+			bannerView = [[GADBannerView alloc] initWithAdSize:kGADAdSizeMediumRectangle];
+			NSLog(@"Medium banner created");
+		} 
+		else if ([size isEqualToString:@"FULL_BANNER"]) 
+		{
+			bannerView = [[GADBannerView alloc] initWithAdSize:kGADAdSizeFullBanner];
+			NSLog(@"Full banner created");
+		} 
+		else if ([size isEqualToString:@"LEADERBOARD"]) 
+		{
+			bannerView = [[GADBannerView alloc] initWithAdSize:kGADAdSizeLeaderboard];
+			NSLog(@"Leaderboard banner created");
+		} 
+		else 
         { //smart banner
-            if (orientation == 0 || orientation == UIInterfaceOrientationPortrait) 
+        	if (orientation == 0 || orientation == UIInterfaceOrientationPortrait) 
             { //portrait
-                bannerView = [[GADBannerView alloc] initWithAdSize:kGADAdSizeSmartBannerPortrait];
-                NSLog(@"Smart portait banner created");
+            	bannerView = [[GADBannerView alloc] initWithAdSize:kGADAdSizeSmartBannerPortrait];
+            	NSLog(@"Smart portait banner created");
             }
             else 
             { //landscape
-                bannerView = [[GADBannerView alloc] initWithAdSize:kGADAdSizeSmartBannerLandscape];
-                NSLog(@"Smart landscape banner created");
+            	bannerView = [[GADBannerView alloc] initWithAdSize:kGADAdSizeSmartBannerLandscape];
+            	NSLog(@"Smart landscape banner created");
             }
         }
         
@@ -91,9 +91,9 @@
 
         if (!testDeviceId.length)
         {
-            request.testDevices = @[testDeviceId];
-            NSLog(@"Using test device with id");
-            NSLog(testDeviceId);
+        	request.testDevices = @[testDeviceId];
+        	NSLog(@"Using test device with id");
+        	NSLog(testDeviceId);
         }
         [bannerView loadRequest :request];
 
@@ -103,80 +103,80 @@
 
 - (void)addBannerViewToView :(UIView *_Nonnull)bannerView :(BOOL)is_on_top
 {
-    bannerView.translatesAutoresizingMaskIntoConstraints = NO;
-    [rootController.view addSubview:bannerView];
-    if (@available(ios 11.0, *)) {
-        [self positionBannerViewFullWidthAtSafeArea:bannerView:is_on_top];
-    } else {
-        [self positionBannerViewFullWidthAtView:bannerView:is_on_top];
-    }
-    Object *obj = ObjectDB::get_instance(instanceId);
-    obj->call_deferred("_on_AdMob_banner_opened");
+	bannerView.translatesAutoresizingMaskIntoConstraints = NO;
+	[rootController.view addSubview:bannerView];
+	if (@available(ios 11.0, *)) {
+		[self positionBannerViewFullWidthAtSafeArea:bannerView:is_on_top];
+	} else {
+		[self positionBannerViewFullWidthAtView:bannerView:is_on_top];
+	}
+	Object *obj = ObjectDB::get_instance(instanceId);
+	obj->call_deferred("_on_AdMob_banner_opened");
 }
 
 
 
 - (void)positionBannerViewFullWidthAtSafeArea :(UIView *_Nonnull)bannerView :(BOOL)is_on_top NS_AVAILABLE_IOS(11.0) {
-    UILayoutGuide *guide = rootController.view.safeAreaLayoutGuide;
-    
-    if (is_on_top) 
-    {
-        [NSLayoutConstraint activateConstraints:@[
-            [guide.leftAnchor constraintEqualToAnchor:bannerView.leftAnchor],
-            [guide.rightAnchor constraintEqualToAnchor:bannerView.rightAnchor],
-            [guide.topAnchor constraintEqualToAnchor:bannerView.topAnchor]
-        ]];
-    }
-    else 
-    {
-        [NSLayoutConstraint activateConstraints:@[
-            [guide.leftAnchor constraintEqualToAnchor:bannerView.leftAnchor],
-            [guide.rightAnchor constraintEqualToAnchor:bannerView.rightAnchor],
-            [guide.bottomAnchor constraintEqualToAnchor:bannerView.bottomAnchor]
-        ]];
-    }
+	UILayoutGuide *guide = rootController.view.safeAreaLayoutGuide;
+	
+	if (is_on_top) 
+	{
+		[NSLayoutConstraint activateConstraints:@[
+			[guide.leftAnchor constraintEqualToAnchor:bannerView.leftAnchor],
+			[guide.rightAnchor constraintEqualToAnchor:bannerView.rightAnchor],
+			[guide.topAnchor constraintEqualToAnchor:bannerView.topAnchor]
+			]];
+	}
+	else 
+	{
+		[NSLayoutConstraint activateConstraints:@[
+			[guide.leftAnchor constraintEqualToAnchor:bannerView.leftAnchor],
+			[guide.rightAnchor constraintEqualToAnchor:bannerView.rightAnchor],
+			[guide.bottomAnchor constraintEqualToAnchor:bannerView.bottomAnchor]
+			]];
+	}
 }
 
 
 - (void)positionBannerViewFullWidthAtView :(UIView *_Nonnull)bannerView :(BOOL)is_on_top 
 {
-    
-    [rootController.view addConstraint :[NSLayoutConstraint constraintWithItem:bannerView
-                                                                    attribute:NSLayoutAttributeLeading
-                                                                    relatedBy:NSLayoutRelationEqual
-                                                                       toItem:rootController.view
-                                                                    attribute:NSLayoutAttributeLeading
-                                                                   multiplier:1
-                                                                     constant:0]];
-    [rootController.view addConstraint :[NSLayoutConstraint constraintWithItem:bannerView
-                                                                    attribute:NSLayoutAttributeTrailing
-                                                                    relatedBy:NSLayoutRelationEqual
-                                                                       toItem:rootController.view
-                                                                    attribute:NSLayoutAttributeTrailing
-                                                                   multiplier:1
-                                                                     constant:0]];
-    
-    if (is_on_top) 
-    {
-        [rootController.view addConstraint :[NSLayoutConstraint constraintWithItem:bannerView
-                                                                        attribute:NSLayoutAttributeTop
-                                                                        relatedBy:NSLayoutRelationEqual
-                                                                           toItem:rootController.topLayoutGuide
-                                                                        attribute:NSLayoutAttributeTop
-                                                                       multiplier:1
-                                                                         constant:0]];
-        
-    }
-    else 
-    {
-        [rootController.view addConstraint :[NSLayoutConstraint constraintWithItem:bannerView
-                                                              attribute:NSLayoutAttributeBottom
-                                                              relatedBy:NSLayoutRelationEqual
-                                                                 toItem:rootController.bottomLayoutGuide
-                                                              attribute:NSLayoutAttributeTop
-                                                             multiplier:1
-                                                               constant:0]];
-    }
+	
+	[rootController.view addConstraint :[NSLayoutConstraint constraintWithItem:bannerView
+		attribute:NSLayoutAttributeLeading
+		relatedBy:NSLayoutRelationEqual
+		toItem:rootController.view
+		attribute:NSLayoutAttributeLeading
+		multiplier:1
+		constant:0]];
+	[rootController.view addConstraint :[NSLayoutConstraint constraintWithItem:bannerView
+		attribute:NSLayoutAttributeTrailing
+		relatedBy:NSLayoutRelationEqual
+		toItem:rootController.view
+		attribute:NSLayoutAttributeTrailing
+		multiplier:1
+		constant:0]];
+	
+	if (is_on_top) 
+	{
+		[rootController.view addConstraint :[NSLayoutConstraint constraintWithItem:bannerView
+			attribute:NSLayoutAttributeTop
+			relatedBy:NSLayoutRelationEqual
+			toItem:rootController.topLayoutGuide
+			attribute:NSLayoutAttributeTop
+			multiplier:1
+			constant:0]];
+		
+	}
+	else 
+	{
+		[rootController.view addConstraint :[NSLayoutConstraint constraintWithItem:bannerView
+			attribute:NSLayoutAttributeBottom
+			relatedBy:NSLayoutRelationEqual
+			toItem:rootController.bottomLayoutGuide
+			attribute:NSLayoutAttributeTop
+			multiplier:1
+			constant:0]];
+	}
 }
 
 - (void)destroy_banner
@@ -187,59 +187,59 @@
 	if (bannerView != nil)
 	{
 		[bannerView setHidden:YES];
-	    [bannerView removeFromSuperview];
-	    bannerView = nil;
+		[bannerView removeFromSuperview];
+		bannerView = nil;
 		Object *obj = ObjectDB::get_instance(instanceId);
-	    obj->call_deferred("_on_AdMob_banner_destroyed");
+		obj->call_deferred("_on_AdMob_banner_destroyed");
 	}
 }
 
 /// Tells the delegate an ad request loaded an ad.
 - (void)adViewDidReceiveAd :(GADBannerView *)adView 
 {
-  NSLog(@"adViewDidReceiveAd");
-  Object *obj = ObjectDB::get_instance(instanceId);
-  obj->call_deferred("_on_AdMob_banner_loaded");
+	NSLog(@"adViewDidReceiveAd");
+	Object *obj = ObjectDB::get_instance(instanceId);
+	obj->call_deferred("_on_AdMob_banner_loaded");
 }
 
 /// Tells the delegate an ad request failed.
 - (void)adView:(GADBannerView *)adView
-    didFailToReceiveAdWithError:(GADRequestError *)error {
-  NSLog(@"adView:didFailToReceiveAdWithError: %@", [error localizedDescription]);
-  Object *obj = ObjectDB::get_instance(instanceId);
-  obj->call_deferred("_on_AdMob_banner_failed_to_load", error.code);  
+didFailToReceiveAdWithError:(GADRequestError *)error {
+	NSLog(@"adView:didFailToReceiveAdWithError: %@", [error localizedDescription]);
+	Object *obj = ObjectDB::get_instance(instanceId);
+	obj->call_deferred("_on_AdMob_banner_failed_to_load", error.code);  
 }
 
 /// Tells the delegate that a full-screen view will be presented in response
 /// to the user clicking on an ad.
 - (void)adViewWillPresentScreen :(GADBannerView *)adView 
 {
-  NSLog(@"adViewWillPresentScreen");
-  Object *obj = ObjectDB::get_instance(instanceId);
-  obj->call_deferred("_on_AdMob_banner_clicked");
+	NSLog(@"adViewWillPresentScreen");
+	Object *obj = ObjectDB::get_instance(instanceId);
+	obj->call_deferred("_on_AdMob_banner_clicked");
 }
 
 /// Tells the delegate that the full-screen view will be dismissed.
 - (void)adViewWillDismissScreen :(GADBannerView *)adView 
 {
-  NSLog(@"adViewWillDismissScreen");
-  Object *obj = ObjectDB::get_instance(instanceId);
-  obj->call_deferred("_on_AdMob_banner_closed");
+	NSLog(@"adViewWillDismissScreen");
+	Object *obj = ObjectDB::get_instance(instanceId);
+	obj->call_deferred("_on_AdMob_banner_closed");
 }
 
 /// Tells the delegate that the full-screen view has been dismissed.
 - (void)adViewDidDismissScreen :(GADBannerView *)adView 
 {
-  NSLog(@"adViewDidDismissScreen");
+	NSLog(@"adViewDidDismissScreen");
 }
 
 /// Tells the delegate that a user click will open another app (such as
 /// the App Store), backgrounding the current app.
 - (void)adViewWillLeaveApplication :(GADBannerView *)adView 
 {
-  NSLog(@"adViewWillLeaveApplication");
-  Object *obj = ObjectDB::get_instance(instanceId);
-  obj->call_deferred("_on_AdMob_banner_left_application");
+	NSLog(@"adViewWillLeaveApplication");
+	Object *obj = ObjectDB::get_instance(instanceId);
+	obj->call_deferred("_on_AdMob_banner_left_application");
 }
 
 
--- admob/ios/src/AdMobRewarded.mm
+++ admob/ios/src/AdMobRewarded.mm
@@ -5,73 +5,73 @@
 
 - (void)dealloc 
 {
-    [rewarded release];
-    [super dealloc];
+	[rewarded release];
+	[super dealloc];
 }
 
 - (void)initialize :(int)instance_id :(NSString*)test_device_id 
 {
-    initialized = true;
-    instanceId = instance_id;
-    testDeviceId = test_device_id;
-    rootController = [AppDelegate getViewController];
+	initialized = true;
+	instanceId = instance_id;
+	testDeviceId = test_device_id;
+	rootController = [AppDelegate getViewController];
 }
 
 
 - (void) load_rewarded :(NSString*)ad_unit_id 
 {
-    NSLog(@"Calling load_rewarded");
-    
-    if ((!initialized) || (!ad_unit_id.length)) 
-    {
-        return;
-    }
+	NSLog(@"Calling load_rewarded");
+	
+	if ((!initialized) || (!ad_unit_id.length)) 
+	{
+		return;
+	}
 
-    rewarded = [[GADRewardedAd alloc] initWithAdUnitID:ad_unit_id];
-    NSLog(@"rewarded created with the id");
-    NSLog(ad_unit_id);
-    GADRequest *request = [GADRequest request];
+	rewarded = [[GADRewardedAd alloc] initWithAdUnitID:ad_unit_id];
+	NSLog(@"rewarded created with the id");
+	NSLog(ad_unit_id);
+	GADRequest *request = [GADRequest request];
 
-    if (!testDeviceId.length)
-    {
-        request.testDevices = @[testDeviceId];
-        NSLog(@"Using test device with id");
-        NSLog(testDeviceId);
-    }
-    [rewarded loadRequest:request completionHandler:^(GADRequestError * _Nullable error) {
-        if (error) 
-        {
-            NSLog(@"error while creating reward");
-            Object *obj = ObjectDB::get_instance(instanceId);
-            obj->call_deferred("_on_AdMob_rewarded_ad_failed_to_show", error.code);
-        }
-        else 
-        {
-            NSLog(@"reward successfully loaded");
-            Object *obj = ObjectDB::get_instance(instanceId);
-            obj->call_deferred("_on_AdMob_rewarded_ad_loaded");
-        }
-    }];
-    
+	if (!testDeviceId.length)
+	{
+		request.testDevices = @[testDeviceId];
+		NSLog(@"Using test device with id");
+		NSLog(testDeviceId);
+	}
+	[rewarded loadRequest :request completionHandler :^(GADRequestError * _Nullable error) {
+		if (error) 
+		{
+			NSLog(@"error while creating reward");
+			Object *obj = ObjectDB::get_instance(instanceId);
+			obj->call_deferred("_on_AdMob_rewarded_ad_failed_to_show", error.code);
+		}
+		else 
+		{
+			NSLog(@"reward successfully loaded");
+			Object *obj = ObjectDB::get_instance(instanceId);
+			obj->call_deferred("_on_AdMob_rewarded_ad_loaded");
+		}
+	}];
+	
 }
 
 - (void) show_rewarded 
 {
-    if (!initialized) 
-    {
-        return;
-    }
+	if (!initialized) 
+	{
+		return;
+	}
 
-    if (rewarded.isReady) 
-    {
-        [rewarded presentFromRootViewController:rootController delegate:self];
-        Object *obj = ObjectDB::get_instance(instanceId);
-        obj->call_deferred("_on_AdMob_rewarded_ad_opened");
-    } 
-    else 
-    {
-        NSLog(@"reward ad wasn't ready");
-    }
+	if (rewarded.isReady) 
+	{
+		[rewarded presentFromRootViewController:rootController delegate:self];
+		Object *obj = ObjectDB::get_instance(instanceId);
+		obj->call_deferred("_on_AdMob_rewarded_ad_opened");
+	} 
+	else 
+	{
+		NSLog(@"reward ad wasn't ready");
+	}
 }
 
 
@@ -79,34 +79,34 @@
 - (void)rewardedAd:(GADRewardedAd *)rewardedAd userDidEarnReward:(GADAdReward *)reward 
 {
   // TODO: Reward the user.
-  NSLog(@"rewardedAd:userDidEarnReward:");
-  NSString *rewardMessage = [NSString stringWithFormat:@"Reward received with currency %@ , amount %lf",
-                                reward.type, [reward.amount doubleValue]];
-  NSLog(rewardMessage);
-  Object *obj = ObjectDB::get_instance(instanceId);
-  obj->call_deferred("_on_AdMob_user_earned_rewarded", [reward.type UTF8String], reward.amount.doubleValue);
+	NSLog(@"rewardedAd:userDidEarnReward:");
+	NSString *rewardMessage = [NSString stringWithFormat:@"Reward received with currency %@ , amount %lf",
+		reward.type, [reward.amount doubleValue]];
+	NSLog(rewardMessage);
+	Object *obj = ObjectDB::get_instance(instanceId);
+	obj->call_deferred("_on_AdMob_user_earned_rewarded", [reward.type UTF8String], reward.amount.doubleValue);
 }
 
 /// Tells the delegate that the rewarded ad was presented.
 - (void)rewardedAdDidPresent:(GADRewardedAd *)rewardedAd 
 {
-  NSLog(@"rewardedAdDidPresent:");
+	NSLog(@"rewardedAdDidPresent:");
 }
 
 /// Tells the delegate that the rewarded ad failed to present.
 - (void)rewardedAd:(GADRewardedAd *)rewardedAd didFailToPresentWithError:(NSError *)error 
 {
-  NSLog(@"rewardedAd:didFailToPresentWithError");
-  Object *obj = ObjectDB::get_instance(instanceId);
-  obj->call_deferred("_on_AdMob_rewarded_ad_failed_to_show", error.code);
+	NSLog(@"rewardedAd:didFailToPresentWithError");
+	Object *obj = ObjectDB::get_instance(instanceId);
+	obj->call_deferred("_on_AdMob_rewarded_ad_failed_to_show", error.code);
 }
 
 /// Tells the delegate that the rewarded ad was dismissed.
 - (void)rewardedAdDidDismiss:(GADRewardedAd *)rewardedAd 
 {
-   NSLog(@"rewardedAdDidDismiss:");
-   Object *obj = ObjectDB::get_instance(instanceId);
-   obj->call_deferred("_on_AdMob_rewarded_ad_closed");
+	NSLog(@"rewardedAdDidDismiss:");
+	Object *obj = ObjectDB::get_instance(instanceId);
+	obj->call_deferred("_on_AdMob_rewarded_ad_closed");
 }
 
 
