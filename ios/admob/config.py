c48994558822545b8d7a4ed86dd97a6d092947d0
Gustavo Maciel <gustamaciel2012@hotmail.com>
2020-11-06 01:15:58 -0300

    Update config.py

--- ios/admob/config.py
+++ ios/admob/config.py
@@ -3,5 +3,5 @@ def can_build(env, plat):
 
 def configure(env):
 	if env['platform'] == 'iphone':
-		env.AppendUnique(FRAMEWORKPATH=['#modules/admob/lib'])
+		env.AppendUnique(FRAMEWORKPATH=['#modules/admob/lib/GoogleMobileAds.xcframework/ios-armv7_arm64'], )
 		env.Append(CPPPATH=['#core'])
