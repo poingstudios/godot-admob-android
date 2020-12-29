199f20820b1331bfb129d5d14da36251786cc5ba
Gustavo Maciel <gustamaciel2012@hotmail.com>
2020-12-29 20:11:26 -0300

    #9 Update

--- ios/admob/src/AdMob.mm
+++ ios/admob/src/AdMob.mm
@@ -34,20 +34,20 @@ void AdMob::initialize(bool is_for_child_directed_treatment, bool is_personalize
         return;
     }
     [GADMobileAds.sharedInstance.requestConfiguration tagForChildDirectedTreatment:is_for_child_directed_treatment];
-    if ([max_ad_content_rating isEqualToString:@"G"]) {
-        [GADMobileAds.sharedInstance.requestConfiguration.maxAdContentRating = GADMaxAdContentRatingGeneral];
+    if ([max_ad_content_rating == "G"]) {
+        GADMobileAds.sharedInstance.requestConfiguration.maxAdContentRating = GADMaxAdContentRatingGeneral;
         NSLog(@"maxAdContentRating = GADMaxAdContentRatingGeneral");
     }
-    else if ([max_ad_content_rating isEqualToString:@"PG"]) {
-        [GADMobileAds.sharedInstance.requestConfiguration.maxAdContentRating = GADMaxAdContentRatingParentalGuidance];
+    else if ([max_ad_content_rating == "PG"]) {
+        GADMobileAds.sharedInstance.requestConfiguration.maxAdContentRating = GADMaxAdContentRatingParentalGuidance;
         NSLog(@"maxAdContentRating = GADMaxAdContentRatingParentalGuidance");
     }
-    else if ([max_ad_content_rating isEqualToString:@"T"]) {
-        [GADMobileAds.sharedInstance.requestConfiguration.maxAdContentRating = GADMaxAdContentRatingTeen];
+    else if ([max_ad_content_rating == "T"]) {
+        GADMobileAds.sharedInstance.requestConfiguration.maxAdContentRating = GADMaxAdContentRatingTeen;
         NSLog(@"maxAdContentRating = GADMaxAdContentRatingTeen");
     }
-    else if ([max_ad_content_rating isEqualToString:@"MA"]) {
-        [GADMobileAds.sharedInstance.requestConfiguration.maxAdContentRating = GADMaxAdContentRatingMatureAudience];
+    else if ([max_ad_content_rating == "MA"]) {
+        GADMobileAds.sharedInstance.requestConfiguration.maxAdContentRating = GADMaxAdContentRatingMatureAudience;
         NSLog(@"maxAdContentRating = GADMaxAdContentRatingMatureAudience");
     }
 
