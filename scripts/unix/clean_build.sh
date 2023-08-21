88bb35abe5d972d51493486444b0acedcba6e06e
Gustavo Maciel <gustamaciel2012@hotmail.com>
2023-08-21 00:54:02 -0300

    ci: create clean_build.sh, download_godot.sh #167

---
+++ scripts/unix/clean_build.sh
@@ -0,0 +1,13 @@
+echo "Executing Clean Build"
+
+if [ $# -eq 0 ]; then
+  echo "Error: Please provide the Godot version as an argument."
+  exit 1
+fi
+
+CURRENT_GODOT_VERSION="$1"
+
+chmod +x ./scripts/unix/download_godot.sh
+./scripts/unix/download_godot.sh "${CURRENT_GODOT_VERSION}" stable
+./gradlew build
+./gradlew zipPlugins -PgodotVersion="${CURRENT_GODOT_VERSION}"
