12f1200851791b90323a9e65b7512d3582785c6b
Gustavo Maciel <gustamaciel2012@hotmail.com>
2025-03-04 13:05:34 -0300

    fix: error while building on GitHub actions

--- scripts/unix/download_godot.sh
+++ scripts/unix/download_godot.sh
@@ -12,20 +12,18 @@ else
   BUILD_VERSION="$2"
 fi
 
+mkdir -p libs/godot-lib
 cd libs/godot-lib
 
 GODOT_AAR_LIB="godot-lib.aar"
-GODOT_AAR_FILENAME="godot-lib.${CURRENT_GODOT_VERSION}.${BUILD_VERSION}"
-FULL_PATHNAME_DOWNLOAD_GODOT_AAR="https://downloads.tuxfamily.org/godotengine/${CURRENT_GODOT_VERSION}"
+GODOT_AAR_FILENAME="godot-lib.${CURRENT_GODOT_VERSION}.${BUILD_VERSION}.template_release.aar"
+FULL_PATHNAME_DOWNLOAD_GODOT_AAR="https://github.com/godotengine/godot-builds/releases/download/${CURRENT_GODOT_VERSION}-${BUILD_VERSION}"
 
-if [ "$BUILD_VERSION" != "stable" ]; then
-  FULL_PATHNAME_DOWNLOAD_GODOT_AAR+="/$BUILD_VERSION"
-fi
-
-GODOT_AAR_FILENAME+=".template_release.aar"
 FULL_PATHNAME_DOWNLOAD_GODOT_AAR+="/$GODOT_AAR_FILENAME"
 
-HTTP_STATUS=$(curl -o "$GODOT_AAR_FILENAME" -w "%{http_code}" "$FULL_PATHNAME_DOWNLOAD_GODOT_AAR")
+echo "FULL_PATHNAME_DOWNLOAD_GODOT_AAR: $FULL_PATHNAME_DOWNLOAD_GODOT_AAR"
+
+HTTP_STATUS=$(curl -L -o "$GODOT_AAR_FILENAME" -w "%{http_code}" "$FULL_PATHNAME_DOWNLOAD_GODOT_AAR")
 
 if [ "$HTTP_STATUS" -eq 200 ]; then
   if [ -f "$GODOT_AAR_LIB" ]; then
