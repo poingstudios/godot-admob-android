4e72bbe443a4db7d1f3e90d28d252be5143fe102
Gustavo Maciel <gustamaciel2012@hotmail.com>
2025-12-26 19:54:45 -0300

    chore: bump version to 3.0.6

--- src/core/src/main/java/com/poingstudios/godot/admob/core/utils/PluginConfiguration.kt
+++ src/core/src/main/java/com/poingstudios/godot/admob/core/utils/PluginConfiguration.kt
@@ -2,6 +2,6 @@ package com.poingstudios.godot.admob.core.utils
 
 class PluginConfiguration {
     object Constants {
-        const val PLUGIN_VERSION = "3.0.5"
+        const val PLUGIN_VERSION = "3.0.6"
     }
 }
