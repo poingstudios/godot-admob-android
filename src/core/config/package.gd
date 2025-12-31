8d7a22fbb85a5ae1b4e65b5f0ff93d44befa9364
Gustavo Maciel <gustamaciel2012@hotmail.com>
2025-12-31 08:37:36 -0300

    build: add core `package.gd` config in export

--- src/core/config/package.gd
+++ src/core/config/package.gd
@@ -1,7 +1,7 @@
 ## Plugin metadata and configuration.
 ##
 ## This file is strictly managed by the CI/CD pipeline.
-## It defines the plugin's identity and ensures version compatibility.
+## [br]It defines the plugin's identity and ensures version compatibility.
 ## [br][b]Note:[/b] This file is READ-ONLY for manual edits during releases.
 
 ## Current version of the plugin in Semantic Versioning (SemVer) format.
