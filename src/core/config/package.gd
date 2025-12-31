50c161b50823c7eff5c94bef7819ae9fbfe5d97d
Gustavo Maciel <gustamaciel2012@hotmail.com>
2025-12-31 08:30:06 -0300

    build: created `package.gd` in `core` subproject

---
+++ src/core/config/package.gd
@@ -0,0 +1,8 @@
+## Plugin metadata and configuration.
+##
+## This file is strictly managed by the CI/CD pipeline.
+## It defines the plugin's identity and ensures version compatibility.
+## [br][b]Note:[/b] This file is READ-ONLY for manual edits during releases.
+
+## Current version of the plugin in Semantic Versioning (SemVer) format.
+const VERSION := "4.0.0"
--- src/core/src/main/java/com/poingstudios/godot/admob/core/utils/PluginConfiguration.kt
+++
@@ -1,8 +0,0 @@
-package com.poingstudios.godot.admob.core.utils
-
-@Suppress("unused") // Used when create a new version in CI/CD.
-class PluginConfiguration {
-    object Constants {
-        const val PLUGIN_VERSION = "4.0.0"
-    }
-}
